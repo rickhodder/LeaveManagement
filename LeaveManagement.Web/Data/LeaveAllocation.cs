8162be9b3bc05ab23d656687ceeb00c85a565a94
Rick Hodder <rickhodder@msn.com>
2022-08-21 19:58:44 -0400

    refactor to base entity, add tables for leave and allocs

---
+++ LeaveManagement.Web/Data/LeaveAllocation.cs
@@ -0,0 +1,10 @@
+﻿namespace LeaveManagement.Web.Data
+{
+    public class LeaveAllocation:BaseEntity
+    {
+        public LeaveType LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+        public string EmployeeId { get; set; }
+
+    }
+}
