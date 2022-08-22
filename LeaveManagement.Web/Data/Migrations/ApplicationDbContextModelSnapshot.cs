a13adba3d571b3616f4c7d886841176db1c8721f
Rick Hodder <rickhodder@msn.com>
2022-08-21 20:04:22 -0400

    Fix login issues

--- LeaveManagement.Web/Data/Migrations/ApplicationDbContextModelSnapshot.cs
+++ LeaveManagement.Web/Data/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -127,7 +127,7 @@ namespace LeaveManagement.Web.Data.Migrations
 
                     b.HasIndex("LeaveTypeId");
 
-                    b.ToTable("LeaveAllocations");
+                    b.ToTable("LeaveAllocations", (string)null);
                 });
 
             modelBuilder.Entity("LeaveManagement.Web.Data.LeaveType", b =>
@@ -153,7 +153,7 @@ namespace LeaveManagement.Web.Data.Migrations
 
                     b.HasKey("Id");
 
-                    b.ToTable("LeaveTypes");
+                    b.ToTable("LeaveTypes", (string)null);
                 });
 
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityRole", b =>
